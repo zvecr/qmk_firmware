3b2362dee0d09c7cc367d46c4585a101eeff7923
zvecr <z.zvecr@gmail.com>
2018-11-19 22:26:55 +0000

    Workaround for other community ortho_4x12 boards failing to build

--- layouts/community/ortho_4x12/zvecr/config.h
+++ layouts/community/ortho_4x12/zvecr/config.h
@@ -9,7 +9,12 @@
 // rgb underglow
 #define RGBLIGHT_SLEEP
 #define RGBLIGHT_ANIMATIONS
+#undef RGBLED_NUM
 #if defined(KEYBOARD_lets_split_rev2)
-  #undef RGBLED_NUM
   #define RGBLED_NUM 10
+#else
+  #define RGBLED_NUM 1
+  #ifndef RGB_DI_PIN
+     #define RGB_DI_PIN D1
+  #endif
 #endif
--- layouts/community/ortho_4x12/zvecr/rules.mk
+++ layouts/community/ortho_4x12/zvecr/rules.mk
@@ -1,11 +1,18 @@
+BOOTMAGIC_ENABLE = lite
 RGBLIGHT_ENABLE = yes
 BACKLIGHT_ENABLE = no
 NKRO_ENABLE = yes
 COMMAND_ENABLE = no
 CONSOLE_ENABLE = no
 MOUSEKEY_ENABLE = no
+AUDIO_ENABLE = no
 
 # for some reason, bootmagic causes lets_split_eh right side to stop working
-ifeq ("$(KEYBOARD)","lets_split")
-  BOOTMAGIC_ENABLE = lite
+ifeq ("$(KEYBOARD)","lets_split_eh")
+  BOOTMAGIC_ENABLE = no
+endif
+
+# disable nkro for V-USB boards
+ifeq ("$(KEYBOARD)","jj40")
+  NKRO_ENABLE = no
 endif
