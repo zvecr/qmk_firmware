ec945dfd369be65e67358ed1f39f08485b93ecf1
zvecr <git@zvecr.com>
2019-02-26 00:47:18 +0000

    Add 75% layout

---
+++ layouts/community/75_iso/zvecr/rules.mk
@@ -0,0 +1,11 @@
+# minimise size to enable as many features as possible
+EXTRAFLAGS        += -flto
+
+BOOTMAGIC_ENABLE = lite
+#RGBLIGHT_ENABLE = yes
+#BACKLIGHT_ENABLE = yes
+NKRO_ENABLE = yes
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+MOUSEKEY_ENABLE = no
+AUDIO_ENABLE = no
