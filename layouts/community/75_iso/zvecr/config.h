ea00880fc1f86311c4de40c03e75187fbde465b0
zvecr <git@zvecr.com>
2019-02-26 00:47:18 +0000

    Add 75% layout

---
+++ layouts/community/75_iso/zvecr/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// # minimise size to enable as many features as possible
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define DISABLE_LEADER
+
+#define TAPPING_TERM 300
+
+// rgb underglow
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_ANIMATIONS
