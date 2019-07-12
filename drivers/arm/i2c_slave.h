516c8a36901c3e4371bebdcaa8fd641618994a4f
zvecr <git@zvecr.com>
2019-07-12 23:17:49 +0100

    Stash i2c_slave WIP

--- drivers/arm/i2c_slave.h
+++ drivers/arm/i2c_slave.h
@@ -1,5 +1,8 @@
-#ifndef I2C_SLAVE_H
-#define I2C_SLAVE_H
+#pragma once
+
+#include "ch.h"
+#include <hal.h>
+#include "i2c_defs.h"
 
 #define I2C_SLAVE_REG_COUNT 30
 
@@ -7,5 +10,3 @@ extern volatile uint8_t i2c_slave_reg[I2C_SLAVE_REG_COUNT];
 
 void i2c_slave_init(uint8_t address);
 void i2c_slave_stop(void);
-
-#endif // I2C_SLAVE_H
