868258a20fd3241f4704f3e499d9e9a3201f95ae
zvecr <git@zvecr.com>
2019-07-28 00:51:34 +0100

    Update keymap to newer functionality

--- layouts/community/ortho_4x12/zvecr/keymap.c
+++ layouts/community/ortho_4x12/zvecr/keymap.c
@@ -1,5 +1,7 @@
 #include QMK_KEYBOARD_H
 
+#include <sendstring_uk.h>
+
 // Layer Declarations
 enum {
     _QWERTY = 0,
@@ -19,6 +21,7 @@ enum {
 
 // Bodge keycodes
 #define KC_CAD LALT(LCTL(KC_DEL))
+#define KC_SNIP LWIN(LSFT(KC_S))
 #define KC_ISOT S(KC_NUHS)
 #define KC_ISOP S(KC_NUBS)
 
@@ -48,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,  \
-  KC_LCTL, KC_LGUI, KC_LALT, META,    LOWER,   RAI_SPC, LOW_SPC, RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT \
+  KC_LCTL, KC_LGUI, KC_LALT, META,    LOWER,   RAI_SPC, LOW_SPC, RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT  \
 ),
 
 /* Lower - symbols
@@ -64,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
-  _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_PIPE, \
+  _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PIPE, \
   _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, _______, KC_ISOT, KC_ISOP, KC_NUHS, KC_PGUP, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_NUBS, KC_HOME, KC_PGDN, KC_END   \
 ),
@@ -89,25 +92,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Meta - funcs + macros
  * ,-----------------------------------------------------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * | CADel|  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 | INT1 | 
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  F13 |  F14 |  F15 |  F16 |  F17 |  F18 |  F19 |  F20 |  F21 |  F22 |  F23 |  F24 |
+ * | Snip |  F11 |  F12 |  F13 |  F14 |  F15 |  F16 |  F17 |  F18 |  F19 |  F20 | INT2 |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |MACRO1|MACRO2|MACRO3|MACRO4|MACRO5| INT1 | INT2 | INT5 |      |      |      |
+ * |      |MACRO1|MACRO2|MACRO3|MACRO4|MACRO5|      |  F21 |  F22 |  F23 |  F24 | INT5 |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_META] =  LAYOUT_ortho_4x12( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  \
-  _______, MACRO_1, MACRO_2, MACRO_3, MACRO_4, MACRO_5, KC_INT1, KC_INT2, KC_INT5, _______, _______, _______, \
+  KC_CAD,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_INT1, \
+  KC_SNIP, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_INT2, \
+  _______, MACRO_1, MACRO_2, MACRO_3, MACRO_4, MACRO_5, _______, KC_F21,  KC_F22,  KC_F23,  KC_F24,  KC_INT5, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 ),
 
 /* Adjust (meta && lower) - dump
  * ,-----------------------------------------------------------------------------------.
- * | Play | Next | Vol- | Vol+ | Mute |      |R Toggl|R Mode|R Rev|R Grad| PrtSc| CADel|
+ * | Play | Next | Vol- | Vol+ | Mute |      |R Toggl|R Mode|R Rev|R Grad| PrtSc|      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |R  HUI|R  SAI|R  VAI|      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -117,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] =  LAYOUT_ortho_4x12( \
-  KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, _______, RGB_TOG, RGB_MOD, RGB_RMOD,RGB_M_G, KC_PSCR, KC_CAD,  \
+  KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, _______, RGB_TOG, RGB_MOD, RGB_RMOD,RGB_M_G, KC_PSCR, _______, \
   _______, _______, _______, _______, _______, _______, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, \
   KC_RSFT, _______, _______, _______, _______, _______, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, \
   KC_RCTRL, _______,KC_RALT, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
@@ -159,16 +162,34 @@ uint32_t layer_state_set_user(uint32_t state) {
   return state;
 }
 
+uint16_t get_tapping_term(uint16_t keycode) {
+  switch (keycode) {
+    case RAI_SPC:
+    case LOW_SPC:
+      return TAPPING_TERM_HOLD;
+    default:
+      return TAPPING_TERM;
+  }
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch(keycode) {
             case MACRO_1:
+                SEND_STRING("z.zvecr@gmail.com");
+                break;
             case MACRO_2:
+                SEND_STRING("info@zvecr.com");
+                break;
             case MACRO_3:
+                SEND_STRING("zvecr@hotmail.co.uk");
+                break;
             case MACRO_4:
+                SEND_STRING("asdf");
+                break;
             case MACRO_5:
-                SEND_STRING("QMK is the best thing ever!");
-                return false;
+                SEND_STRING("password");
+                break;
         }
     }
     return true;
