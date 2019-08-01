eb8bc066a660752d360258cf0d8e204e22503fd6
zvecr <git@zvecr.com>
2019-08-01 23:26:20 +0100

    Update 75 keymap

--- layouts/community/75_iso/zvecr/rules.mk
+++ layouts/community/75_iso/zvecr/rules.mk
@@ -1,11 +1,9 @@
-# minimise size to enable as many features as possible
-EXTRAFLAGS        += -flto
-
 BOOTMAGIC_ENABLE = lite
-#RGBLIGHT_ENABLE = yes
-#BACKLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = yes
+BACKLIGHT_ENABLE = no
 NKRO_ENABLE = yes
 COMMAND_ENABLE = no
 CONSOLE_ENABLE = no
 MOUSEKEY_ENABLE = no
 AUDIO_ENABLE = no
+LINK_TIME_OPTIMIZATION_ENABLE = yes
