4fae62d54378a64ed4f4a850b8475527fdcad0b8
zvecr <git@zvecr.com>
2019-08-12 23:07:13 +0100

    Stash initial split_blackpill files

---
+++ keyboards/handwired/split_blackpill/split_blackpill.c
@@ -0,0 +1,31 @@
+#include "split_blackpill.h"
+
+bool is_keyboard_master(void) {
+    #pragma message("bodge for keyboard master detection")
+
+    // #define SPLIT_MASTER_PIN B12
+  
+    // setPinInputHigh(SPLIT_MASTER_PIN);
+    // return readPin(SPLIT_MASTER_PIN);
+    static uint8_t is_master = 0xff;
+    if(is_master == 0xff) {
+
+        is_master = false;
+        for(uint8_t i = 0; i < 25; i++) {
+            if (usbGetDriverStateI(&USBD1) == USB_ACTIVE) {
+                is_master = true;
+                break;
+            }
+
+  //dprintf("is_keyboard_master::is_master false %d\n");
+            wait_ms(100);
+        }
+    }
+    return !!is_master;
+}
+
+void keyboard_pre_init_kb(void){
+    palSetLineMode(C13, PAL_MODE_OUTPUT_OPENDRAIN);
+    palSetLineMode(B9, PAL_MODE_OUTPUT_OPENDRAIN);
+    palSetLineMode(B8, PAL_MODE_OUTPUT_OPENDRAIN);
+}
