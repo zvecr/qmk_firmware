2ed39ac5abf22534e4f759143f52ab9461131c65
zvecr <git@zvecr.com>
2020-01-21 23:12:37 +0000

    Clean up ready for PR

--- quantum/backlight/backlight_arm.c
+++ quantum/backlight/backlight_arm.c
@@ -30,9 +30,13 @@
 #    define PWM_OUTPUT_MODE PWM_OUTPUT_ACTIVE_LOW;
 #endif
 
+
+#ifdef BACKLIGHT_BREATHING
 static void breathing_callback(PWMDriver *pwmp);
-static inline void breathing_max(void);
-static inline void breathing_min(void);
+static void breathing_reset(uint8_t level);
+#else
+#    define breathing_callback NULL
+#endif
 
 static PWMConfig pwmCFG = {0xFFFF,             /* PWM clock frequency  */
                            256,                /* PWM period (in ticks) 1S (1/10kHz=0.1mS 0.1ms*10000 ticks=1S) */
@@ -62,8 +66,6 @@ static uint16_t cie_lightness(uint16_t v) {
 }
 
 void backlight_init_ports(void) {
-    // printf("backlight_init_ports()\n");
-
 #ifdef USE_GPIOV1
     palSetPadMode(PAL_PORT(BACKLIGHT_PIN), PAL_PAD(BACKLIGHT_PIN), PAL_MODE_STM32_ALTERNATE_PUSHPULL);
 #else
@@ -86,23 +88,18 @@ void backlight_init_ports(void) {
 void backlight_set(uint8_t level) {
     if (level > BACKLIGHT_LEVELS) level = BACKLIGHT_LEVELS;
 
-    // printf("backlight_set(%d)\n", level);
     if (level == 0) {
         // Turn backlight off
         pwmDisableChannel(&BACKLIGHT_PWM_DRIVER, BACKLIGHT_PWM_CHANNEL - 1);
-#ifdef BACKLIGHT_BREATHING
-        breathing_min();
-#endif
     } else {
         // Turn backlight on
         uint32_t duty = (uint32_t)(cie_lightness(0xFFFF * (uint32_t)level / BACKLIGHT_LEVELS));
-        // printf("duty: (%d)\n", duty);
         pwmEnableChannel(&BACKLIGHT_PWM_DRIVER, BACKLIGHT_PWM_CHANNEL - 1, PWM_FRACTION_TO_WIDTH(&BACKLIGHT_PWM_DRIVER, 0xFFFF, duty));
+    }
 
 #ifdef BACKLIGHT_BREATHING
-        breathing_max();
+    breathing_reset(level);
 #endif
-    }
 }
 
 void backlight_task(void) {}
@@ -116,12 +113,14 @@ void backlight_task(void) {}
  */
 static const uint8_t breathing_table[BREATHING_STEPS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 17, 20, 24, 28, 32, 36, 41, 46, 51, 57, 63, 70, 76, 83, 91, 98, 106, 113, 121, 129, 138, 146, 154, 162, 170, 178, 185, 193, 200, 207, 213, 220, 225, 231, 235, 240, 244, 247, 250, 252, 253, 254, 255, 254, 253, 252, 250, 247, 244, 240, 235, 231, 225, 220, 213, 207, 200, 193, 185, 178, 170, 162, 154, 146, 138, 129, 121, 113, 106, 98, 91, 83, 76, 70, 63, 57, 51, 46, 41, 36, 32, 28, 24, 20, 17, 15, 12, 10, 8, 6, 5, 4, 3, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-static bool     breathing         = false;
+static bool breathing = false;
 static uint16_t breathing_counter = 0;
 
 static inline void breathing_min(void) { breathing_counter = 0; }
 
 static inline void breathing_max(void) { breathing_counter = get_breathing_period() * 256 / 2; }
+
+static void breathing_reset(uint8_t level) { level ? breathing_max() : breathing_min(); }
 
 bool is_breathing(void) { return breathing; }
 
@@ -133,7 +132,6 @@ void breathing_enable(void) {
 void breathing_disable(void) {
     breathing = false;
 
-    // printf("breathing_disable()\n");
     pwmDisablePeriodicNotificationI(&BACKLIGHT_PWM_DRIVER);
 
     // Restore backlight level
@@ -142,22 +140,17 @@ void breathing_disable(void) {
 
 void breathing_toggle(void) { is_breathing() ? breathing_disable() : breathing_enable(); }
 
-void breathing_pulse(void) {
-    // Just call the common stuff....
-    backlight_pulse();
-}
 
 // Use this before the cie_lightness function.
 static inline uint16_t scale_backlight(uint16_t v) { return v / BACKLIGHT_LEVELS * get_backlight_level(); }
 
 static void breathing_callback(PWMDriver *pwmp) {
-    uint8_t  breathing_period = get_breathing_period();
-    uint16_t interval         = (uint16_t)breathing_period * 256 / BREATHING_STEPS;
+    uint8_t breathing_period = get_breathing_period();
+    uint16_t interval = (uint16_t)breathing_period * 256 / BREATHING_STEPS;
 
     // resetting after one period to prevent ugly reset at overflow.
     breathing_counter = (breathing_counter + 1) % (breathing_period * 256);
-    uint8_t index     = breathing_counter / interval % BREATHING_STEPS;
-
+    uint8_t index = breathing_counter / interval % BREATHING_STEPS;
     uint32_t duty = cie_lightness(scale_backlight(breathing_table[index] * 256));
 
     chSysLockFromISR();
@@ -165,6 +158,9 @@ static void breathing_callback(PWMDriver *pwmp) {
     chSysUnlockFromISR();
 }
 
-#else
-static void breathing_callback(PWMDriver *pwmp) { (void)pwmp; }
+// TODO: remove
+void breathing_pulse(void) {
+    backlight_pulse();
+}
+
 #endif
