5df408fa4817442ebc98a7d49896bede79d4f752
zvecr <git@zvecr.com>
2020-01-21 23:12:49 +0000

    Stash files

--- keyboards/keebio/levinson/rev2_proton_c/config.h
+++ keyboards/keebio/levinson/rev2_proton_c/config.h
@@ -63,7 +63,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Backlight LEDs */
 #define BACKLIGHT_PIN B0
 #define BACKLIGHT_LEVELS 7
-//#define BACKLIGHT_BREATHING
+#define BACKLIGHT_BREATHING
 
 #define BACKLIGHT_PWM_DRIVER PWMD3
 #define BACKLIGHT_PWM_CHANNEL 3
