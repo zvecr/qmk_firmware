23b7aa5b3aa44df041fe14a284b3f4cda017c9a2
zvecr <git@zvecr.com>
2022-02-07 19:56:01 +0000

    Update to latest riot - 2022.01

--- platforms/riot/drivers/i2c_master.c
+++ platforms/riot/drivers/i2c_master.c
@@ -18,7 +18,7 @@
 #undef i2c_init
 
 #ifndef I2C_BUS_ID
-#    define I2C_BUS_ID    (I2C_DEV(0))
+#    define I2C_BUS_ID (I2C_DEV(0))
 #endif
 
 __attribute__((weak)) void _i2c_init(void) {
@@ -26,23 +26,16 @@ __attribute__((weak)) void _i2c_init(void) {
 }
 
 i2c_status_t _i2c_start(uint8_t devaddr, uint16_t timeout) {
-    return (i2c_acquire(I2C_BUS_ID) == 0) ? I2C_STATUS_SUCCESS : I2C_STATUS_ERROR;
+    i2c_acquire(I2C_BUS_ID);
+    return I2C_STATUS_SUCCESS;
 }
 
-i2c_status_t _i2c_transmit(uint8_t devaddr, const uint8_t* data, uint16_t length, uint16_t timeout) {
-    return (i2c_write_bytes(I2C_BUS_ID, (devaddr >> 1), data, length, 0) == 0) ? I2C_STATUS_SUCCESS : I2C_STATUS_ERROR;
-}
+i2c_status_t _i2c_transmit(uint8_t devaddr, const uint8_t* data, uint16_t length, uint16_t timeout) { return (i2c_write_bytes(I2C_BUS_ID, (devaddr >> 1), data, length, 0) == 0) ? I2C_STATUS_SUCCESS : I2C_STATUS_ERROR; }
 
-i2c_status_t _i2c_receive(uint8_t devaddr, uint8_t* data, uint16_t length, uint16_t timeout) {
-    return (i2c_read_bytes(I2C_BUS_ID, (devaddr >> 1), data, length, 0) == 0) ? I2C_STATUS_SUCCESS : I2C_STATUS_ERROR;
-}
+i2c_status_t _i2c_receive(uint8_t devaddr, uint8_t* data, uint16_t length, uint16_t timeout) { return (i2c_read_bytes(I2C_BUS_ID, (devaddr >> 1), data, length, 0) == 0) ? I2C_STATUS_SUCCESS : I2C_STATUS_ERROR; }
 
-i2c_status_t _i2c_writeReg(uint8_t devaddr, uint8_t regaddr, const uint8_t* data, uint16_t length, uint16_t timeout) {
-    return (i2c_write_regs(I2C_BUS_ID, (devaddr >> 1), regaddr, data, length, 0) == 0) ? I2C_STATUS_SUCCESS : I2C_STATUS_ERROR;
-}
+i2c_status_t _i2c_writeReg(uint8_t devaddr, uint8_t regaddr, const uint8_t* data, uint16_t length, uint16_t timeout) { return (i2c_write_regs(I2C_BUS_ID, (devaddr >> 1), regaddr, data, length, 0) == 0) ? I2C_STATUS_SUCCESS : I2C_STATUS_ERROR; }
 
-i2c_status_t _i2c_readReg(uint8_t devaddr, uint8_t regaddr, uint8_t* data, uint16_t length, uint16_t timeout) {
-    return (i2c_read_regs(I2C_BUS_ID, (devaddr >> 1), regaddr, data, length, 0) == 0) ? I2C_STATUS_SUCCESS : I2C_STATUS_ERROR;
-}
+i2c_status_t _i2c_readReg(uint8_t devaddr, uint8_t regaddr, uint8_t* data, uint16_t length, uint16_t timeout) { return (i2c_read_regs(I2C_BUS_ID, (devaddr >> 1), regaddr, data, length, 0) == 0) ? I2C_STATUS_SUCCESS : I2C_STATUS_ERROR; }
 
 void _i2c_stop(void) { i2c_release(I2C_BUS_ID); }
--- tmk_core/protocol/riot/usb_driver.c
+++ tmk_core/protocol/riot/usb_driver.c
@@ -9,13 +9,33 @@
 #include "usb_driver.h"
 #include "report.h"
 
-static uint8_t keyboard_led_state = 0;
-uint8_t        usbdrv_keyboard_leds(void) { return keyboard_led_state; }
+// Disable any logging - lines are kept to minimise diffs
+#undef DEBUG
+#define DEBUG(...)
 
+// **********************************************
+// Partially based on:
+//   lib/riot/sys/include/usb/usbus/hid.h
+
+/**
+ * @brief Composite HID device context struct forward declaration
+ */
 typedef struct comp_hid_device comp_hid_device_t;
 
+/**
+ * @brief HID data callback.
+ *
+ * Callback for received data from the USB host
+ *
+ * @param[in]   hid     HID handler context
+ * @param[in]   data    ptr to the data
+ * @param[in]   len     Length of the received data
+ */
 typedef void (*comp_hid_cb_t)(comp_hid_device_t *hid, uint8_t *data, size_t len);
 
+/**
+ * @brief Composite HID device context struct
+ */
 struct comp_hid_device {
     usbus_handler_t        handler_ctrl; /**< control handler */
     usbus_interface_t      iface;        /**< HID interface */
@@ -28,7 +48,216 @@ struct comp_hid_device {
     comp_hid_cb_t          cb;           /**< Callback for data handlers */
     event_t                tx_ready;     /**< Transmit ready event */
     mutex_t                in_lock;      /**< mutex used for locking hid send */
+
+    /**
+     * @brief Host to device data buffer
+     */
+    usbdev_ep_buf_t out_buf[CONFIG_USBUS_HID_INTERRUPT_EP_SIZE];
+
+    /**
+     * @brief Device to host data buffer
+     */
+    usbdev_ep_buf_t in_buf[CONFIG_USBUS_HID_INTERRUPT_EP_SIZE];
 };
+
+// **********************************************
+// Partially based on:
+//   lib/riot/sys/usb/usbus/hid/hid.c
+
+static void _init(usbus_t *usbus, usbus_handler_t *handler);
+static void _event_handler(usbus_t *usbus, usbus_handler_t *handler, usbus_event_usb_t event);
+static int  _control_handler(usbus_t *usbus, usbus_handler_t *handler, usbus_control_request_state_t state, usb_setup_t *setup);
+static void _transfer_handler(usbus_t *usbus, usbus_handler_t *handler, usbdev_ep_t *ep, usbus_event_transfer_t event);
+
+static void _handle_tx_ready(event_t *ev);
+
+static const usbus_handler_driver_t hid_driver = {
+    .init             = _init,             //
+    .event_handler    = _event_handler,    //
+    .control_handler  = _control_handler,  //
+    .transfer_handler = _transfer_handler  //
+};
+
+static size_t _gen_hid_descriptor(usbus_t *usbus, void *arg);
+
+static const usbus_descr_gen_funcs_t _hid_descriptor = {
+    .fmt_post_descriptor = _gen_hid_descriptor,                    //
+    .len                 = {.fixed_len = sizeof(usb_desc_hid_t)},  //
+    .len_type            = USBUS_DESCR_LEN_FIXED                   //
+};
+
+static size_t _gen_hid_descriptor(usbus_t *usbus, void *arg) {
+    comp_hid_device_t *hid_dev = arg;
+
+    usb_desc_hid_t hid_desc;
+    hid_desc.length        = sizeof(usb_desc_hid_t);
+    hid_desc.desc_type     = USB_HID_DESCR_HID;
+    hid_desc.bcd_hid       = USB_HID_VERSION_BCD;
+    hid_desc.country_code  = USB_HID_COUNTRY_CODE_NOTSUPPORTED;
+    hid_desc.num_descrs    = 0x01;
+    hid_desc.report_type   = USB_HID_DESCR_REPORT;
+    hid_desc.report_length = hid_dev->conf.report_desc_size;
+
+    usbus_control_slicer_put_bytes(usbus, (uint8_t *)&hid_desc, sizeof(hid_desc));
+    return sizeof(usb_desc_hid_t);
+}
+
+static void _handle_tx_ready(event_t *ev) {
+    comp_hid_device_t *hid = container_of(ev, comp_hid_device_t, tx_ready);
+
+    usbdev_ep_xmit(hid->ep_in->ep, hid->in_buf, hid->occupied);
+}
+
+static void usbdrv_init_interface(usbus_t *usbus, comp_hid_device_t *hid, comp_hid_cb_t cb, comp_hid_device_conf_t *conf) {
+    hid->usbus = usbus;
+    mutex_init(&hid->in_lock);
+    hid->handler_ctrl.driver = &hid_driver;
+    hid->cb                  = cb;
+
+    memcpy(&hid->conf, conf, sizeof(comp_hid_device_conf_t));
+
+    DEBUG("hid_init: %d %d \n", report_desc_size, report_desc[0]);
+    usbus_register_event_handler(usbus, &hid->handler_ctrl);
+}
+
+static void _init(usbus_t *usbus, usbus_handler_t *handler) {
+    DEBUG("USB_HID: initialization\n");
+    comp_hid_device_t *hid = container_of(handler, comp_hid_device_t, handler_ctrl);
+
+    hid->tx_ready.handler = _handle_tx_ready;
+
+    hid->hid_descr.next  = NULL;
+    hid->hid_descr.funcs = &_hid_descriptor;
+    hid->hid_descr.arg   = hid;
+
+    /*
+       Configure Interface as USB_HID interface
+     */
+    hid->iface.class     = USB_CLASS_HID;
+    hid->iface.subclass  = hid->conf.subclass;
+    hid->iface.protocol  = hid->conf.protocol;
+    hid->iface.descr_gen = &hid->hid_descr;
+    hid->iface.handler   = handler;
+
+    /* IN endpoint to send data to host */
+    hid->ep_in = usbus_add_endpoint(usbus, &hid->iface, USB_EP_TYPE_INTERRUPT, USB_EP_DIR_IN, hid->conf.ep_size);
+
+    /* interrupt endpoint polling rate in ms */
+    hid->ep_in->interval = hid->conf.interval;
+
+    usbus_enable_endpoint(hid->ep_in);
+
+    usbus_add_interface(usbus, &hid->iface);
+}
+
+static void _event_handler(usbus_t *usbus, usbus_handler_t *handler, usbus_event_usb_t event) {
+    (void)usbus;
+    (void)handler;
+
+    switch (event) {
+        default:
+            DEBUG("USB HID unhandled event: 0x%x\n", event);
+            break;
+    }
+}
+
+report_keyboard_t keyboard_report_sent                          = {{0}};
+uint8_t           keyboard_idle __attribute__((aligned(2)))     = 0;
+uint8_t           keyboard_protocol __attribute__((aligned(2))) = 1;
+
+static uint8_t keyboard_led_state = 0;
+uint8_t        usbdrv_keyboard_leds(void) { return keyboard_led_state; }
+
+static int _control_handler(usbus_t *usbus, usbus_handler_t *handler, usbus_control_request_state_t state, usb_setup_t *setup) {
+    comp_hid_device_t *hid = (comp_hid_device_t *)handler;
+
+    DEBUG("USB_HID: request: %d type: %d value: %d length: %d state: %d \n", setup->request, setup->type, setup->value >> 8, setup->length, state);
+
+    /* Requests defined in USB HID 1.11 spec section 7 */
+    switch (setup->request) {
+        case USB_SETUP_REQ_GET_DESCRIPTOR: {
+            uint8_t desc_type = setup->value >> 8;
+            if (desc_type == USB_HID_DESCR_REPORT) {
+                usbus_control_slicer_put_bytes(usbus, hid->conf.report_desc, hid->conf.report_desc_size);
+                // Bodge to get windows to get past "IRP USBD_STATUS: USBD_STATUS_CANCELED"
+                uint8_t status[2] = {0};
+                usbus_control_slicer_put_bytes(usbus, status, sizeof(status));
+            } else if (desc_type == USB_HID_DESCR_HID) {
+                _gen_hid_descriptor(usbus, hid);
+            }
+            break;
+        }
+        case USB_HID_REQUEST_GET_REPORT:
+            usbus_control_slicer_put_bytes(usbus, (uint8_t *)&keyboard_report_sent, sizeof(keyboard_report_sent));
+            break;
+        case USB_HID_REQUEST_GET_IDLE:
+            if (setup->index == KEYBOARD_INTERFACE) {
+                usbus_control_slicer_put_bytes(usbus, &keyboard_idle, sizeof(keyboard_idle));
+            }
+            break;
+        case USB_HID_REQUEST_GET_PROTOCOL:
+            if (setup->index == KEYBOARD_INTERFACE) {
+                usbus_control_slicer_put_bytes(usbus, &keyboard_protocol, sizeof(keyboard_protocol));
+            }
+            break;
+        case USB_HID_REQUEST_SET_REPORT:
+            if ((state == USBUS_CONTROL_REQUEST_STATE_OUTDATA)) {
+                size_t   size = 0;
+                uint8_t *data = usbus_control_get_out_data(usbus, &size);
+                if (hid->conf.protocol == USB_HID_PROTOCOL_KEYBOARD) {  // KEYBOARD_INTERFACE || NKRO_INTERFACE
+                    if (size == 2) {
+                        keyboard_led_state = data[1];
+                    } else if (size == 1) {
+                        keyboard_led_state = data[0];
+                    }
+                } else {
+                    if (size > 0) {
+                        hid->cb(hid, data, size);
+                    }
+                }
+            }
+            break;
+        case USB_HID_REQUEST_SET_IDLE:
+            if (setup->index == KEYBOARD_INTERFACE) {
+                keyboard_idle = ((setup->value & 0xFF00) >> 8);
+            }
+            break;
+        case USB_HID_REQUEST_SET_PROTOCOL:
+            if (setup->index == KEYBOARD_INTERFACE) {
+                keyboard_protocol = (setup->value & 0xFF);
+            }
+            break;
+        default:
+            DEBUG("USB_HID: unknown request %d \n", setup->request);
+            return -1;
+    }
+    return 1;
+}
+
+static void _transfer_handler(usbus_t *usbus, usbus_handler_t *handler, usbdev_ep_t *ep, usbus_event_transfer_t event) {
+    (void)usbus;
+    (void)event;
+    DEBUG("USB_HID: transfer_handler\n");
+
+    comp_hid_device_t *hid = (comp_hid_device_t *)handler;
+
+    if ((ep->dir == USB_EP_DIR_IN) && (ep->type == USB_EP_TYPE_INTERRUPT)) {
+        mutex_unlock(&hid->in_lock);
+        hid->occupied = 0;
+    } else if ((ep->dir == USB_EP_DIR_OUT) && (ep->type == USB_EP_TYPE_INTERRUPT)) {
+        size_t len;
+        usbdev_ep_get(ep, USBOPT_EP_AVAILABLE, &len, sizeof(size_t));
+        if (len > 0) {
+            hid->cb(hid, hid->out_buf, len);
+        }
+        usbdev_ep_xmit(ep, hid->out_buf, CONFIG_USBUS_HID_INTERRUPT_EP_SIZE);
+    }
+}
+
+// **********************************************
+// **********************************************
+// Partially based on:
+//   lib/riot/sys/usb/usbus/hid/hid_io.c
 
 static uint8_t   _hid_rx_buf_mem[CONFIG_USBUS_HID_INTERRUPT_EP_SIZE];
 static isrpipe_t _hid_stdio_isrpipe = ISRPIPE_INIT(_hid_rx_buf_mem);
@@ -48,177 +277,7 @@ static void _hid_rx_pipe(comp_hid_device_t *dev, uint8_t *data, size_t len) {
 #endif
 }
 
-static size_t _gen_hid_descriptor(usbus_t *usbus, void *arg) {
-    comp_hid_device_t *dev = arg;
-
-    usb_desc_hid_t hid_desc;
-    hid_desc.length        = sizeof(usb_desc_hid_t);
-    hid_desc.desc_type     = USB_HID_DESCR_HID;
-    hid_desc.bcd_hid       = USB_HID_VERSION_BCD;
-    hid_desc.country_code  = USB_HID_COUNTRY_CODE_NOTSUPPORTED;
-    hid_desc.num_descrs    = 0x01;
-    hid_desc.report_type   = USB_HID_DESCR_REPORT;
-    hid_desc.report_length = dev->conf.report_desc_size;
-
-    usbus_control_slicer_put_bytes(usbus, (uint8_t *)&hid_desc, sizeof(hid_desc));
-    return sizeof(usb_desc_hid_t);
-}
-
-static const usbus_descr_gen_funcs_t _hid_descriptor = {
-    .fmt_post_descriptor = _gen_hid_descriptor,                    //
-    .len                 = {.fixed_len = sizeof(usb_desc_hid_t)},  //
-    .len_type            = USBUS_DESCR_LEN_FIXED                   //
-};
-
-static void _handle_tx_ready(event_t *ev) {
-    comp_hid_device_t *dev = container_of(ev, comp_hid_device_t, tx_ready);
-
-    usbdev_ep_ready(dev->ep_in->ep, dev->occupied);
-}
-
-static void _init(usbus_t *usbus, usbus_handler_t *handler) {
-    comp_hid_device_t *dev = container_of(handler, comp_hid_device_t, handler_ctrl);
-
-    dev->tx_ready.handler = _handle_tx_ready;
-
-    dev->hid_descr.next  = NULL;
-    dev->hid_descr.funcs = &_hid_descriptor;
-    dev->hid_descr.arg   = dev;
-
-    dev->iface.class     = USB_CLASS_HID;
-    dev->iface.subclass  = dev->conf.subclass;
-    dev->iface.protocol  = dev->conf.protocol;
-    dev->iface.descr_gen = &dev->hid_descr;
-    dev->iface.handler   = handler;
-
-    /* IN endpoint to send data to host */
-    dev->ep_in = usbus_add_endpoint(usbus, &dev->iface, USB_EP_TYPE_INTERRUPT, USB_EP_DIR_IN, dev->conf.ep_size);
-
-    dev->ep_in->interval = dev->conf.interval;
-
-    usbus_enable_endpoint(dev->ep_in);
-
-    usbus_add_interface(usbus, &dev->iface);
-    // usbus_handler_set_flag(handler, USBUS_HANDLER_FLAG_RESET);
-}
-
-static void _event_handler(usbus_t *usbus, usbus_handler_t *handler, usbus_event_usb_t event) {
-    (void)usbus;
-    (void)handler;
-
-    switch (event) {
-        default:
-            break;
-    }
-}
-
-report_keyboard_t keyboard_report_sent                          = {{0}};
-uint8_t           keyboard_idle __attribute__((aligned(2)))     = 0;
-uint8_t           keyboard_protocol __attribute__((aligned(2))) = 1;
-
-static int _control_handler(usbus_t *usbus, usbus_handler_t *handler, usbus_control_request_state_t state, usb_setup_t *setup) {
-    comp_hid_device_t *dev = (comp_hid_device_t *)handler;
-
-    /* Requests defined in USB HID 1.11 spec section 7 */
-    switch (setup->request) {
-        case USB_SETUP_REQ_GET_DESCRIPTOR: {
-            uint8_t desc_type = setup->value >> 8;
-            if (desc_type == USB_HID_DESCR_REPORT) {
-                usbus_control_slicer_put_bytes(usbus, dev->conf.report_desc, dev->conf.report_desc_size);
-                // Bodge to get windows to get past "IRP USBD_STATUS: USBD_STATUS_CANCELED"
-                uint8_t status[2] = {0};
-                usbus_control_slicer_put_bytes(usbus, status, sizeof(status));
-            } else if (desc_type == USB_HID_DESCR_HID) {
-                _gen_hid_descriptor(usbus, dev);
-            }
-            break;
-        }
-        case USB_HID_REQUEST_GET_REPORT:
-            usbus_control_slicer_put_bytes(usbus, (uint8_t *)&keyboard_report_sent, sizeof(keyboard_report_sent));
-            break;
-        case USB_HID_REQUEST_SET_REPORT:
-            if ((state == USBUS_CONTROL_REQUEST_STATE_OUTDATA)) {
-                size_t   size = 0;
-                uint8_t *data = usbus_control_get_out_data(usbus, &size);
-                if (dev->conf.protocol == USB_HID_PROTOCOL_KEYBOARD) {  // KEYBOARD_INTERFACE || NKRO_INTERFACE
-                    if (size == 2) {
-                        keyboard_led_state = data[1];
-                    } else if (size == 1) {
-                        keyboard_led_state = data[0];
-                    }
-                } else {
-                    if (size > 0) {
-                        dev->cb(dev, data, size);
-                    }
-                }
-            }
-            break;
-        case USB_HID_REQUEST_GET_PROTOCOL:
-            if (setup->index == KEYBOARD_INTERFACE) {
-                usbus_control_slicer_put_bytes(usbus, &keyboard_protocol, sizeof(keyboard_protocol));
-            }
-            break;
-        case USB_HID_REQUEST_SET_PROTOCOL:
-            if (setup->index == KEYBOARD_INTERFACE) {
-                keyboard_protocol = (setup->value & 0xFF);
-            }
-            break;
-        case USB_HID_REQUEST_GET_IDLE:
-            if (setup->index == KEYBOARD_INTERFACE) {
-                usbus_control_slicer_put_bytes(usbus, &keyboard_idle, sizeof(keyboard_idle));
-            }
-            break;
-        case USB_HID_REQUEST_SET_IDLE:
-            if (setup->index == KEYBOARD_INTERFACE) {
-                keyboard_idle = ((setup->value & 0xFF00) >> 8);
-            }
-            break;
-        default:
-            return -1;
-    }
-    return 1;
-}
-
-static void _transfer_handler(usbus_t *usbus, usbus_handler_t *handler, usbdev_ep_t *ep, usbus_event_transfer_t event) {
-    (void)usbus;
-    (void)event;
-
-    comp_hid_device_t *dev = (comp_hid_device_t *)handler;
-
-    if ((ep->dir == USB_EP_DIR_IN) && (ep->type == USB_EP_TYPE_INTERRUPT)) {
-        mutex_unlock(&dev->in_lock);
-        dev->occupied = 0;
-    } else if ((ep->dir == USB_EP_DIR_OUT) && (ep->type == USB_EP_TYPE_INTERRUPT)) {
-        size_t len;
-        usbdev_ep_get(ep, USBOPT_EP_AVAILABLE, &len, sizeof(size_t));
-        if (len > 0) {
-            dev->cb(dev, ep->buf, len);
-        }
-        usbdev_ep_ready(ep, 0);
-    }
-}
-
-static const usbus_handler_driver_t hid_driver = {
-    .init             = _init,             //
-    .event_handler    = _event_handler,    //
-    .control_handler  = _control_handler,  //
-    .transfer_handler = _transfer_handler  //
-};
-
-static comp_hid_device_t hid[TOTAL_INTERFACES];
-
-void usbdrv_init_interface(usbus_t *usbus, comp_hid_device_conf_t *conf) {
-    comp_hid_device_t *dev = &hid[conf->id];
-
-    mutex_init(&dev->in_lock);
-    dev->usbus               = usbus;
-    dev->handler_ctrl.driver = &hid_driver;
-    dev->cb                  = _hid_rx_pipe;
-
-    memcpy(&dev->conf, conf, sizeof(comp_hid_device_conf_t));
-
-    usbus_register_event_handler(usbus, &dev->handler_ctrl);
-}
+static comp_hid_device_t hid_interfaces[TOTAL_INTERFACES];
 
 usbus_t g_usbus;
 
@@ -228,7 +287,7 @@ void usbdrv_init(comp_hid_device_conf_t *config, size_t len) {
     usbus_init(&g_usbus, usbdev);
 
     for (uint8_t id = 0; id < len; id++) {
-        usbdrv_init_interface(&g_usbus, &config[id]);
+        usbdrv_init_interface(&g_usbus, &hid_interfaces[id], _hid_rx_pipe, &config[id]);
     }
 
     static char _stack[USBUS_STACKSIZE];
@@ -236,31 +295,27 @@ void usbdrv_init(comp_hid_device_conf_t *config, size_t len) {
 }
 
 void usbdrv_write(uint8_t id, const void *buffer, size_t len) {
-    comp_hid_device_t *dev = &hid[id];
+    comp_hid_device_t *hid = &hid_interfaces[id];
 
-    uint8_t *buffer_ep = dev->ep_in->ep->buf;
-    uint16_t max_size  = dev->ep_in->maxpacketsize;
+    uint8_t *buffer_ep = hid->in_buf;
+    uint16_t max_size  = hid->ep_in->maxpacketsize;
     size_t   offset    = 0;
 
+    // TODO: Implement timeout???
     while (len) {
-        mutex_lock(&dev->in_lock);
-        // TODO: Implement timeout???
-        // uint8_t locked = mutex_trylock(&dev->in_lock);
-        // if(locked == 0)
-        //     return;
-
+        mutex_lock(&hid->in_lock);
         if (len > max_size) {
             memmove(buffer_ep + offset, (uint8_t *)buffer + offset, max_size);
             offset += max_size;
-            dev->occupied = max_size;
+            hid->occupied = max_size;
             len -= max_size;
         } else {
             memmove(buffer_ep + offset, (uint8_t *)buffer + offset, len);
             offset += len;
-            dev->occupied = len;
+            hid->occupied = len;
             len           = 0;
         }
-        usbus_event_post(dev->usbus, &dev->tx_ready);
+        usbus_event_post(hid->usbus, &hid->tx_ready);
     }
 }
 
