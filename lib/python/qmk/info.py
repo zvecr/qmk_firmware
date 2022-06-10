f204cbac6b1d81dd900aa3d41aed180b6b715f40
zvecr <git@zvecr.com>
2022-06-10 05:17:42 +0100

    Promote missing matrix data to api error

--- lib/python/qmk/info.py
+++ lib/python/qmk/info.py
@@ -807,8 +807,11 @@ def merge_info_jsons(keyboard, info_data):
                     for new_key, existing_key in zip(layout['layout'], info_data['layouts'][layout_name]['layout']):
                         existing_key.update(new_key)
             else:
-                layout['c_macro'] = False
-                info_data['layouts'][layout_name] = layout
+                if not all('matrix' in key_data.keys() or len(key_data) == 2 or all(isinstance(n, int) for n in key_data) for key_data in layout['layout']):
+                    _log_error(info_data, 'Layout "%s" has no "matrix" definition in either "info.json" or "<keyboard>.h"!' % layout_name)
+                else:
+                    layout['c_macro'] = False
+                    info_data['layouts'][layout_name] = layout
 
         # Update info_data with the new data
         if 'layouts' in new_info_data:
