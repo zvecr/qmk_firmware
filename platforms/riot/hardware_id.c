a9a04a3ee4d790a6de284ff2441a4d1164d0255a
zvecr <git@zvecr.com>
2022-06-13 22:09:25 +0100

    implement get_hardware_id

---
+++ platforms/riot/hardware_id.c
@@ -0,0 +1,14 @@
+// Copyright 2022 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "hardware_id.h"
+#include "periph/cpuid.h"
+
+hardware_id_t get_hardware_id(void) {
+    hardware_id_t id = {0};
+#if CPUID_LEN
+    static_assert(sizeof(id) >= CPUID_LEN, "CPUID oversized for hardware_id_t");
+    cpuid_get(&id);
+#endif
+    return id;
+}
