63cf76f028d664bb125ee4ff336da4473c00dcd4
zvecr <git@zvecr.com>
2023-01-26 03:22:09 +0000

    Implement ATOMIC_RESTORESTATE

--- platforms/riot/atomic_util.h
+++ platforms/riot/atomic_util.h
@@ -1,37 +1,29 @@
-/* Copyright 2021 QMK
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
 #include <irq.h>
 
 static __inline__ uint8_t __interrupt_disable__(void) {
-    irq_disable();
-
-    return 1;
+    return irq_disable();
 }
 
-static __inline__ void __interrupt_enable__(const uint8_t *__s) {
+static __inline__ void __interrupt_enable__(const unsigned *__s) {
     irq_enable();
 
     __asm__ volatile("" ::: "memory");
     (void)__s;
 }
 
-#define ATOMIC_BLOCK(type) for (type, __ToDo = __interrupt_disable__(); __ToDo; __ToDo = 0)
-#define ATOMIC_FORCEON uint8_t sreg_save __attribute__((__cleanup__(__interrupt_enable__))) = 0
+static __inline__ void __interrupt_restore__(const unsigned *__s) {
+    irq_restore(*__s);
 
-#define ATOMIC_BLOCK_RESTORESTATE _Static_assert(0, "ATOMIC_BLOCK_RESTORESTATE not implemented")
+    __asm__ volatile("" ::: "memory");
+}
+
+#define ATOMIC_BLOCK(type) for (type, __ToDo = 1; __ToDo; __ToDo = 0)
+#define ATOMIC_FORCEON unsigned status_save __attribute__((__cleanup__(__interrupt_enable__))) = __interrupt_disable__()
+#define ATOMIC_RESTORESTATE unsigned status_save __attribute__((__cleanup__(__interrupt_restore__))) = __interrupt_disable__()
+
+#define ATOMIC_BLOCK_RESTORESTATE ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
 #define ATOMIC_BLOCK_FORCEON ATOMIC_BLOCK(ATOMIC_FORCEON)
