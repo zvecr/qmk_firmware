629dbafc14f59a3fca01e081b5107c1930921b66
zvecr <git@zvecr.com>
2023-02-04 11:03:56 +0000

    Fix nrf RESET

--- platforms/riot/bootloaders/adafruit_nrf52.c
+++ platforms/riot/bootloaders/adafruit_nrf52.c
@@ -4,9 +4,6 @@
 #include "usb_board_reset.h"
 #include "periph/pm.h"
 
-// reuse logic from reset.c where possible - might not be implemented
-__attribute__((weak)) void usb_board_reset_in_bootloader(void) {}
-
 __attribute__((weak)) void bootloader_jump(void) {
     usb_board_reset_in_bootloader();
 }
